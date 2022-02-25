0ab4ef6886a25e8b8dbd5fdbfb69a3e5d5600045
Shubham Vaish <68528688+Chitrak07@users.noreply.github.com>
2022-02-25 19:43:58 +0530

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,85 @@
+
+from flask import Flask,render_template,request
+import os
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from werkzeug.utils import secure_filename
+import streamlit as st
+from PIL import Image
+import numpy as np
+import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html = True)
+
+st.title('Plant Pathology')
+
+def main() :
+    file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
+    if file_uploaded is not None :
+        image = Image.open(file_uploaded)
+        st.write("Uploaded Image.")
+        figure = plt.figure()
+        plt.imshow(image)
+        plt.axis('off')
+        st.pyplot(figure)
+        result = predict_class(image)
+        st.write('Prediction : {}'.format(result))
+
+def predict_class(image) :
+    with st.spinner('Loading Model...'):
+        classifier_model = keras.models.load_model(r'C:\Users\deepa\Desktop\pp\apple3.h5', compile = False)
+
+    shape = ((224,224,3))
+    model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape = shape)])
+    test_image = image.resize((224, 224))
+    test_image = keras.preprocessing.image.img_to_array(test_image)
+    test_image /= 255.0
+    test_image = np.expand_dims(test_image, axis = 0)
+
+    result = model.predict(test_image)
+    categories = ['Healthy', 'Multiple Disease', 'Rust', 'Scab']
+    pred_class = result.argmax()
+    output = categories[pred_class]
+    return output
+
+footer = """<style>
+a:link , a:visited{
+    color: white;
+    background-color: transparent;
+    text-decoration: None;
+}
+
+a:hover,  a:active {
+    color: red;
+    background-color: transparent;
+    text-decoration: None;
+}
+
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: transparent;
+    color: black;
+    text-align: center;
+}
+</style>
+
+<div class="footer">
+<p align="center"> <a href="#">Developed  by Shubham Vaish</a></p>
+</div>
+        """
+
+st.markdown(footer, unsafe_allow_html = True)
+
+if __name__ == '__main__' :
+    main()
